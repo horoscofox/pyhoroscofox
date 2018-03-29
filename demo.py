a6b0283874e657d2b38d6287926e55d546e99f7d
Andrea Stagi <stagi.andrea@gmail.com>
2018-03-29 23:52:43 +0200

    Add demo

---
+++ demo.py
@@ -0,0 +1,18 @@
+from horoscofox import paolo
+
+
+print ('♏️  Scorpio Today')
+scorpio_today = paolo.scorpio.today()
+print (scorpio_today.date_start)
+print (scorpio_today.date_end)
+print (scorpio_today.text)
+
+print ('♏️  Scorpio Tomorrow')
+scorpio_tomorrow = paolo.scorpio.tomorrow()
+print (scorpio_tomorrow.date_start)
+print (scorpio_tomorrow.date_end)
+print (scorpio_tomorrow.text)
+
+print ('♍️  Virgo Today')
+virgo_today = paolo.get(sign='virgo', kind='today')
+print (virgo_today.text)
